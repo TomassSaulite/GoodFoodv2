4c85299f17a59535199abe0797c3beefa3778fa5
Semitis <samantajuste344@gmail.com>
2024-06-27 02:20:48 +0300

    Renamed a few files

--- GoodFoodv2/GoodFoodv2/urls.py
+++ GoodFoodv2/GoodFoodv2/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from client.views import Index,Login,Register,Empty
+from client.views import Index,UserLogin,UserRegister,Empty
+from restaurant.views import Login,Register,Empty
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
-    path('login', Login.as_view(), name='login'),
-    path('register', Register.as_view(), name='register'),
+    path('user/login', UserLogin.as_view(), name='userLogin'),
+    path('restaurant/login', Login.as_view(), name='login'),
+    path('user/register', UserRegister.as_view(), name='userRegister'),
+    path('restaurant/register', Register.as_view(), name='register'),
     path('empty', Empty.as_view(), name='empty'),
 ]
--- GoodFoodv2/restaurant/views.py
+++ GoodFoodv2/restaurant/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
+from django.views import View
+from .forms import CustomAuthenticationForm, UserRegistrationForm
+from django.contrib import messages
 
-# Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'Index.html')
+class Empty(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'empty.html')
+    
+class UserLogin(View):
+    def get(self, request, *args, **kwargs):
+        form = CustomAuthenticationForm()
+        return render(request, 'userLogin.html', {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = CustomAuthenticationForm()
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                print("!!!!!!!!!!!")
+                return redirect('register')
+            else:
+                messages.error(request, 'Invalid email or password.')
+                
+        # return render(request, 'empty.html', {'form': form})
+        return redirect('register')
+    
+class UserRegister(View):
+    def get(self, request, *args, **kwargs):
+        form = UserRegistrationForm()
+        return render(request, 'userRegister.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data['password1']
+            user.set_password(password)
+            user.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Please correct the error below.')
+        return render(request, 'register.html', {'form': form})
+
